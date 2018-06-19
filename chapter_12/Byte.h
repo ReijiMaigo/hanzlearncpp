73bdf66aa91daf112be2aa5f73d27123a5b8334c
NGAN WEI HAN-GQC638 <weihan.ngan1@motorolasolutions.com>
2018-06-19 18:10:11 +0800

    update chapter_12

--- chapter_12/Byte.h
+++ chapter_12/Byte.h
@@ -1,4 +1,137 @@
 #ifndef BYTE_H
 #define BYTE_H
-
-#
+#include <iostream>
+// Member functions (implicit "this"):
+class Byte {
+    unsigned char b;
+    public:
+    Byte(unsigned char bb = 0) : b(bb) {}
+    // No side effects: const member function:
+    const Byte
+        operator+(const Byte& right) const {
+        return Byte(b + right.b);
+    }
+    const Byte
+        operator-(const Byte& right) const {
+        return Byte(b - right.b);
+    }
+    const Byte
+        operator*(const Byte& right) const {
+        return Byte(b * right.b);
+    }
+    const Byte
+        operator/(const Byte& right) const {
+        return Byte(b / right.b);
+    }
+    const Byte
+        operator%(const Byte& right) const {
+        return Byte(b % right.b);
+    }
+    const Byte
+        operator^(const Byte& right) const {
+        return Byte(b ^ right.b);
+    }
+    const Byte
+        operator&(const Byte& right) const {
+        return Byte(b & right.b);
+    }
+    const Byte
+        operator|(const Byte& right) const {
+        return Byte(b | right.b);
+    }
+    const Byte
+        operator<<(const Byte& right) const {
+        return Byte(b << right.b);
+    }
+    const Byte
+        operator>>(const Byte& right) const {
+        return Byte(b >> right.b);
+    }
+    // Assignments modify & return lvalue.
+    // operator= can only be a member function:
+    Byte& operator=(const Byte& right) {
+        // Handle self-assignment:
+        if(this == &right) return *this;
+        b = right.b;
+        return *this;
+    }
+    Byte& operator+=(const Byte& right) {
+        if(this == &right) {/* self-assignment */}
+        b += right.b;
+        return *this;
+    }
+    Byte& operator-=(const Byte& right) {
+        if(this == &right) {/* self-assignment */}
+        b -= right.b;
+        return *this;
+    }
+    Byte& operator*=(const Byte& right) {
+        if(this == &right) {/* self-assignment */}
+        b *= right.b;
+        return *this;
+    }
+    Byte& operator/=(const Byte& right) {
+        if(this == &right) {/* self-assignment */}
+        b /= right.b;
+        return *this;
+    }
+    Byte& operator%=(const Byte& right) {
+        if(this == &right) {/* self-assignment */}
+        b %= right.b;
+        return *this;
+    }
+    Byte& operator^=(const Byte& right) {
+        if(this == &right) {/* self-assignment */}
+        b ^= right.b;
+        return *this;
+    }
+    Byte& operator&=(const Byte& right) {
+        if(this == &right) {/* self-assignment */}
+        b &= right.b;
+        return *this;
+    }
+    Byte& operator|=(const Byte& right) {
+        if(this == &right) {/* self-assignment */}
+        b |= right.b;
+        return *this;
+    }
+    Byte& operator>>=(const Byte& right) {
+        if(this == &right) {/* self-assignment */}
+        b >>= right.b;
+        return *this;
+    }
+    Byte& operator<<=(const Byte& right) {
+        if(this == &right) {/* self-assignment */}
+        b <<= right.b;
+        return *this;
+    }
+    // Conditional operators return true/false:
+    int operator==(const Byte& right) const {
+        return b == right.b;
+    }
+    int operator!=(const Byte& right) const {
+        return b != right.b;
+    }
+    int operator<(const Byte& right) const {
+        return b < right.b;
+    }
+    int operator>(const Byte& right) const {
+        return b > right.b;
+    }
+    int operator<=(const Byte& right) const {
+        return b <= right.b;
+    }
+    int operator>=(const Byte& right) const {
+        return b >= right.b;
+    }
+    int operator&&(const Byte& right) const {
+        return b && right.b;
+    }
+    int operator||(const Byte& right) const {
+        return b || right.b;
+    }
+    void print(std::ostream& os) const {
+        os << "0x" << std::hex << int(b) << std::dec;
+    }
+};
+#endif // BYTE_H ///:~
